201d78750e0ba0e13b8594de85f69323d80b6a41
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-08-21 00:08:48 +0100

    beginning of boilerplate

---
+++ cmd/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	//GET http://host:port/dataset?query=..
+	//POST http://178.62.59.88:31392/mondial/query
+
+	//POST http://host:port/dataset
+	//Content-type: application/sparql-query
+//	b := `SELECT ?subject ?predicate ?object
+//WHERE {
+//  ?subject ?predicate ?object
+//}
+//LIMIT 25`
+
+	b := `SELECT ?subject ?predicate ?object
+WHERE {
+  ?subject <http://dooodle/predicate/hasColumn> ?object .
+}
+LIMIT 200`
+
+	body := bytes.NewReader([]byte(b))
+
+	//req, err := http.NewRequest("POST", "http://178.62.59.88:31392/mondial/query", body)
+	req, err := http.NewRequest("POST", "http://178.62.59.88:31392/mondial", body)
+	req.Header.Set("Content-type","application/sparql-query")
+	//req.Header.Set("Accept", "text/csv")
+	//req.Header.Set("Accept","application/sparql-results+json")
+	req.Header.Set("Accept","text/tab-separated-values")
+	//req.Header.Set("Accept", "application/sparql-results+xml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//req.SetBasicAuth("","")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	//data := bytes.Buffer{}
+	//io.Copy(&data,resp.Body)
+	io.Copy(os.Stdout,resp.Body)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dooodle/vis-pattern
+
+go 1.12
