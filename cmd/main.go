d30bd1998ee53525d1fb338166b35b6802e77d71
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-08-23 00:13:31 +0100

    add scatter and bubble vis patterns

--- cmd/main.go
+++ cmd/main.go
@@ -8,41 +8,42 @@ import (
 	"os"
 )
 
+type Format int
+
+const (
+	Csv Format = iota
+	Tsv
+	Json
+	Xml
+)
+
+var currentFormat = Csv
+var sparqlUrl = "http://178.62.59.88:31392/mondial"
+var keyLimit = "100"
+
 func main() {
+	writeBarchartMapping(os.Stdout)
+	writeScatterMapping(os.Stdout)
+	writeBubbleMapping(os.Stdout)
+}
 
-	//GET http://host:port/dataset?query=..
-	//POST http://178.62.59.88:31392/mondial/query
-
-	//POST http://host:port/dataset
-	//Content-type: application/sparql-query
-//	b := `SELECT ?subject ?predicate ?object
-//WHERE {
-//  ?subject ?predicate ?object
-//}
-//LIMIT 25`
-
-	b := `SELECT * WHERE {
-  ?sub <http://dooodle/predicate/hasColumn> ?obj .
-  ?sub <http://dooodle/predicate/hasColumn> ?key .
+func writeBarchartMapping(w io.Writer) {
+	b := `SELECT ?entity ?key ?scalar WHERE {
+  ?entity <http://dooodle/predicate/hasColumn> ?scalar .
+  ?entity <http://dooodle/predicate/hasColumn> ?key .
   ?key <http://dooodle/predicate/numDistinct> ?num .
-  ?obj <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
-  FILTER (?num > 0 && ?num <= 100)
+  ?scalar <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  FILTER (?num > 0 && ?num <= `+ keyLimit +`)
+  FILTER (?key != ?scalar)
 } 
 LIMIT 200`
-
 	body := bytes.NewReader([]byte(b))
-
-	//req, err := http.NewRequest("POST", "http://178.62.59.88:31392/mondial/query", body)
-	req, err := http.NewRequest("POST", "http://178.62.59.88:31392/mondial", body)
-	req.Header.Set("Content-type","application/sparql-query")
-	//req.Header.Set("Accept", "text/csv")
-	//req.Header.Set("Accept","application/sparql-results+json")
-	req.Header.Set("Accept","text/tab-separated-values")
-	//req.Header.Set("Accept", "application/sparql-results+xml")
+	req, err := http.NewRequest("POST", sparqlUrl, body)
+	req.Header.Set("Content-type", "application/sparql-query")
+	req.Header.Set("Accept", mimeFormat(currentFormat))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	//req.SetBasicAuth("","")
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
@@ -50,8 +51,76 @@ LIMIT 200`
 	}
 
 	defer resp.Body.Close()
-	//data := bytes.Buffer{}
-	//io.Copy(&data,resp.Body)
-	io.Copy(os.Stdout,resp.Body)
+	io.Copy(w, resp.Body)
+}
 
+func writeScatterMapping(w io.Writer) {
+	b := `SELECT ?entity ?scalarA ?scalarB WHERE {
+  ?entity <http://dooodle/predicate/hasColumn> ?scalarA .
+  ?entity <http://dooodle/predicate/hasColumn> ?scalarB .
+  ?scalarA <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  ?scalarB <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  FILTER (?scalarA != ?scalarB)
+} 
+LIMIT 200`
+	body := bytes.NewReader([]byte(b))
+	req, err := http.NewRequest("POST", sparqlUrl, body)
+	req.Header.Set("Content-type", "application/sparql-query")
+	req.Header.Set("Accept", mimeFormat(currentFormat))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//req.SetBasicAuth("","")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	io.Copy(w, resp.Body)
+}
+
+func writeBubbleMapping(w io.Writer) {
+	b := `SELECT ?entity ?scalarA ?scalarB ?scalarC WHERE {
+  ?entity <http://dooodle/predicate/hasColumn> ?scalarA .
+  ?entity <http://dooodle/predicate/hasColumn> ?scalarB .
+  ?entity <http://dooodle/predicate/hasColumn> ?scalarC .
+  ?scalarA <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  ?scalarB <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  ?scalarC <http://dooodle/predicate/hasDimension> <http://dooodle/dimension/scalar> .
+  FILTER (?scalarA != ?scalarB)
+  FILTER (?scalarA != ?scalarC)
+  FILTER (?scalarB != ?scalarC)
+} 
+LIMIT 200`
+	body := bytes.NewReader([]byte(b))
+	req, err := http.NewRequest("POST", sparqlUrl, body)
+	req.Header.Set("Content-type", "application/sparql-query")
+	req.Header.Set("Accept", mimeFormat(currentFormat))
+	if err != nil {
+		log.Fatal(err)
+	}
+	//req.SetBasicAuth("","")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+	io.Copy(w, resp.Body)
+}
+
+func mimeFormat(cf Format) string {
+	switch cf {
+	case Csv:
+		return "text/csv"
+	case Json:
+		return "application/sparql-results+json"
+	case Tsv:
+		return "text/tab-separated-values"
+	case Xml:
+		return "application/sparql-results+xml"
+	default:
+		return "application/sparql-results+xml"
+	}
 }
